e575c9825b5b629c6c44b43d939feedc052ab74a
Anji-91 <128667706+Anji-91@users.noreply.github.com>
2024-11-09 10:18:46 +0530

    Add files via upload

---
+++ graphicturtle-3.py
@@ -0,0 +1,23 @@
+import turtle
+
+colors= ['red','purple','blue','green','orange','yellow',]
+
+t= turtle.Pen()
+
+turtle.bgcolor('black')
+
+t.speed(50)
+
+for x in range(300):
+
+        t.pencolor(colors[x%6])
+
+        t.width(x/100+1)
+
+        t.forward(x)
+
+        t.left(59)
+
+turtle.hideturtle()
+
+turtle.mainloop()
